b861f10a60682550c0b4ce1d38668170cbcd10db
Jay Sabherwal <jaysabherwal@gmail.com>
2023-10-13 21:57:18 +0100

    Start new home page

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,25 +1,39 @@
-import './App.css';
-import { Route, Routes } from 'react-router-dom';
-import Home from './screens/home';
-import Game from './screens/game';
-import { useEffect, useRef, useState } from 'react';
-import { Message } from './models/message';
-import { Action } from './models/action';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./screens/home";
+import Game from "./screens/game";
+import { useEffect, useRef, useState } from "react";
+import { Message } from "./models/message";
+import { Alert, Snackbar } from "@mui/material";
+
+const isLocal = () => process.env.ENVIRONMENT === 'local';
 
 function App() {
   const [messages, setMessages] = useState<Message[]>([]);
-  const webSocket = useRef<WebSocket>()
-  
-  useEffect(() => {
-    webSocket.current = new WebSocket('ws://localhost:8081');
-    webSocket.current.onmessage = (message) => {
-      const obj = JSON.parse(message.data);
-      const mes = {
-        action: Action[obj.action],
-        data: obj.data
-      };
-      setMessages(prev => [...prev, mes]);
+  const webSocket = useRef<WebSocket>();
+  const [open, setOpen] = useState(false);
+
+  const handleClose = (
+    event: React.SyntheticEvent | Event,
+    reason?: string
+  ) => {
+    if (reason === "clickaway") {
+      return;
     }
+
+    setOpen(false);
+  };
+
+  useEffect(() => {
+    webSocket.current = new WebSocket(isLocal() ? "ws://localhost:8081" : `${window.location.protocol}//${window.location.host}`);
+    webSocket.current.onerror = () => {
+      setOpen(true)
+    };
+    webSocket.current.onmessage = (message) => {
+      const mes = JSON.parse(message.data) as Message;
+
+      setMessages((prev) => [...prev, mes]);
+    };
     return () => webSocket.current?.close();
   }, []);
 
@@ -32,12 +46,27 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Home messages={messages} sendMessage={sendMessage}/>} />
-        <Route path="/game/:id" element={<Game messages={messages} sendMessage={sendMessage}/>} />
+        <Route
+          path="/"
+          element={<Home messages={messages} sendMessage={sendMessage} />}
+        />
+        <Route
+          path="/game/:id"
+          element={<Game messages={messages} sendMessage={sendMessage} />}
+        />
       </Routes>
+      <Snackbar
+        open={open}
+        autoHideDuration={6000}
+        onClose={handleClose}
+        message="Note archived"
+      >
+        <Alert onClose={handleClose} severity="error" sx={{ width: "100%" }}>
+          Error connecting to server!
+        </Alert>
+      </Snackbar>
     </div>
   );
 }
-
 
 export default App;
--- website/src/screens/home/home.tsx
+++ website/src/screens/home/home.tsx
@@ -1,111 +1,123 @@
-import { ContentCopy } from '@mui/icons-material';
-import { Button, Card, CircularProgress, Container, Grid, IconButton, Paper, TextField, Typography } from '@mui/material';
-import { useCallback, useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { Action } from '../../models/action';
-import { Message } from '../../models/message';
+import { ContentCopy } from "@mui/icons-material";
+import {
+  Button,
+  CircularProgress,
+  Grid,
+  IconButton,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { useCallback, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Action } from "../../models/action";
+import { Message } from "../../models/message";
+import styled from "styled-components";
 
 interface HomeProps {
-    messages: Message[];
-    sendMessage: (message: Message) => void;
+  messages: Message[];
+  sendMessage: (message: Message) => void;
 }
 
 export function Home({ messages, sendMessage }: HomeProps) {
-    let navigate = useNavigate();
+  let navigate = useNavigate();
 
-    const [loading, setLoading] = useState(false);
-    const [hasCreated, setCreated] = useState(false);
-    const [displayMessage, setDisplayMessage] = useState('Start playing using the button below!');
-    const [url, setURL] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [hasCreated, setCreated] = useState(false);
+  const [displayMessage, setDisplayMessage] = useState(
+    "Start playing using the button below!"
+  );
+  const [url, setURL] = useState("");
 
-    const handleJoin = useCallback((data) => {
-        const game = {
-            id: data.id
-        }
-        navigate(`/game/${data.id}`, { state: { game } });
-    }, [navigate]);
+  const handleJoin = useCallback(
+    (data) => {
+      const game = {
+        id: data.id,
+      };
+      navigate(`/game/${data.id}`, { state: { game } });
+    },
+    [navigate]
+  );
 
-    useEffect(() => {
-        const latestMessage = messages[messages.length - 1];
+  useEffect(() => {
+    const latestMessage = messages[messages.length - 1];
 
-        switch (latestMessage?.action) {
-            case Action.CREATE:
-                handleCreate(latestMessage.data);
-                break;
-            case Action.JOIN:
-                handleJoin(latestMessage.data);
-                break;
-        }
-    }, [messages, handleJoin]);
-
-    const handleCreate = (data) => {
-        setDisplayMessage('Game created, your opponent can join using the link below');
-        setURL(`${window.location.protocol}//${window.location.host}/game/${data.id}`);
-        setCreated(true);
-    };
-
-    const handleCreateClick = () => {
-        setLoading(true);
-        const message = {
-            action: Action.CREATE,
-            data: ''
-        }
-        send(message);
-    };
-
-    const send = (data: Message) => {
-        sendMessage(data);
-    };
-
-    const handleCopy = () => {
-        navigator.clipboard.writeText(url)
+    switch (latestMessage?.action) {
+      case Action.CREATE:
+        handleCreate(latestMessage.data);
+        break;
+      case Action.JOIN:
+        handleJoin(latestMessage.data);
+        break;
     }
+  }, [messages, handleJoin]);
 
-    const paperSx = {
-        p: 2,
-        margin: 'auto',
-        width: '40%',
-        maxHeight: '50%',
-        flexGrow: 1,
-        backgroundColor: '#808080'
-    }
+  const StyledCreateButton = styled(Button)``;
 
-    return (
-        <>
-            <Paper sx={paperSx}>
-                <Grid container direction="column" justifyContent="center" alignItems="center">
-                    <Grid item>
-                        <Typography>
-                            Four
-                        </Typography>
-                    </Grid>
-                    <Grid item>
-                        <Typography>
-                            {displayMessage}
-                        </Typography>
-                    </Grid>
-                    <Grid item>
-                        {hasCreated && (
-                            <span>
-                                <TextField label="URL" size="small" variant="outlined" defaultValue={url} disabled />
-                                <IconButton aria-label="copy" onClick={handleCopy}>
-                                    <ContentCopy/>
-                                </IconButton>
-                            </span>
-                        )}
-                    </Grid>
-                    <Grid item>
-                        {loading && (<CircularProgress size={24}></CircularProgress>)}
-                    </Grid>
-                    <Grid item>
-                        <Button variant="contained" disabled={loading || hasCreated} onClick={handleCreateClick}>
-                            <Typography>
-                                Create Game
-                            </Typography>
-                        </Button>
-                    </Grid>
-                </Grid>
-            </Paper>
-        </>
+  const handleCreate = (data) => {
+    setDisplayMessage(
+      "Game created, your opponent can join using the link below"
     );
+    setURL(
+      `${window.location.protocol}//${window.location.host}/game/${data.id}`
+    );
+    setCreated(true);
+  };
+
+  const handleCreateClick = () => {
+    setLoading(true);
+    const message = {
+      action: Action.CREATE,
+      data: "",
+    };
+    send(message);
+  };
+
+  const send = (data: Message) => {
+    sendMessage(data);
+  };
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText(url);
+  };
+
+  return (
+    <>
+      <Grid container justifyContent="center" alignItems="center" spacing={2}>
+        <Grid item xs={12}>
+          <Typography>Four</Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography>{displayMessage}</Typography>
+        </Grid>
+
+        {hasCreated && (
+          <Grid item xs={4} md={2}>
+            <span>
+              <TextField
+                label="URL"
+                size="small"
+                variant="outlined"
+                defaultValue={url}
+                disabled
+              />
+              <IconButton aria-label="copy" onClick={handleCopy}>
+                <ContentCopy />
+              </IconButton>
+            </span>
+          </Grid>
+        )}
+        <Grid item xs={4} md={2}>
+          <StyledCreateButton
+            variant="contained"
+            disabled={loading || hasCreated}
+            onClick={handleCreateClick}
+            fullWidth={true}
+          >
+            {loading && <CircularProgress size={24}></CircularProgress>}
+            {!loading && <Typography>Create Game</Typography>}
+          </StyledCreateButton>
+        </Grid>
+      </Grid>
+    </>
+  );
 }
